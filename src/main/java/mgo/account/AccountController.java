cb72a02492597f08071a1ba5c3bd79ca92de23e3
Shazi199 <shazi199@gmail.com>
2017-08-15 16:24:15 +0800

    去掉httpclient，改用自带的httpkit

--- src/main/java/mgo/account/AccountController.java
+++ src/main/java/mgo/account/AccountController.java
@@ -1,18 +1,11 @@
 package mgo.account;
 
-import java.io.IOException;
 import java.util.Date;
-
-import org.apache.http.HttpEntity;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 
 import com.alibaba.fastjson.JSONObject;
 import com.jfinal.aop.Clear;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.HttpKit;
 import com.jfinal.kit.PropKit;
 
 import mgo.model.User;
@@ -88,32 +81,24 @@ public class AccountController extends Controller {
 	@Clear(CheckLoginInterceptor.class)
 	public void weiboLogin() {
 		String code = getPara("code");
+		String url = "https://api.weibo.com/oauth2/access_token?client_id=" + PropKit.get("weibo_appid")
+		+ "&client_secret=" + PropKit.get("weibo_secretid") + "&grant_type=authorization_code"
+		+ "&redirect_uri=http://" + PropKit.get("weibo_url") + "/account/weiboLogin" + "&code=" + code;
 
-		HttpClient client = HttpClients.createDefault();
-		HttpPost post = new HttpPost("https://api.weibo.com/oauth2/access_token?client_id=" + PropKit.get("weibo_appid")
-				+ "&client_secret=" + PropKit.get("weibo_secretid") + "&grant_type=authorization_code"
-				+ "&redirect_uri=http://" + PropKit.get("weibo_url") + "/account/weiboLogin" + "&code=" + code);
-		try {
-			HttpResponse response = client.execute(post);
-			HttpEntity entity = response.getEntity();
-			String content = EntityUtils.toString(entity);
+		String content = HttpKit.post(url, null);
 
-			JSONObject object = JSONObject.parseObject(content);
-			String uid = object.getString("uid");
-			User u = new User().findByOauth(uid, "weibo");
+		JSONObject object = JSONObject.parseObject(content);
+		String uid = object.getString("uid");
+		User u = new User().findByOauth(uid, "weibo");
 
-			if (u != null) {
-				u.setLastlogin(new Date());
-				u.update();
-				setSessionAttr("currentUser", u);
+		if (u != null) {
+			u.setLastlogin(new Date());
+			u.update();
+			setSessionAttr("currentUser", u);
 
-				redirect("/");
-			} else {
-				renderText("未关联有效账户");
-			}
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			redirect("/");
+		} else {
+			renderText("未关联有效账户");
 		}
 	}
 }
