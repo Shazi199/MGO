67a19b849644976c4baad813b98fa0a773111fe3
Shazi199 <shazi199@gmail.com>
2019-01-03 17:16:38 +0800

    代码结构调整

--- src/main/java/tool/ModelGenerator.java
+++ src/main/java/tool/ModelGenerator.java
@@ -2,14 +2,15 @@ package tool;
 
 import com.jfinal.kit.PropKit;
 import com.jfinal.plugin.activerecord.generator.Generator;
-import com.jfinal.plugin.c3p0.C3p0Plugin;
+import com.jfinal.plugin.druid.DruidPlugin;
 
 public class ModelGenerator {
 	public static void main(String[] args) {
 		PropKit.use("config.txt");
-		C3p0Plugin plugin = new C3p0Plugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password"));
+		DruidPlugin plugin = new DruidPlugin(PropKit.get("jdbcUrl"), PropKit.get("user"), PropKit.get("password"));
 		plugin.start();
-		Generator gen = new Generator(plugin.getDataSource(), "mgo.model", "src/main/java/mgo/model/", "mgo.model", "src/main/java/mgo/model/");
+		Generator gen = new Generator(plugin.getDataSource(), "mgo.model.base", "src/main/java/mgo/model/base/", "mgo.model", "src/main/java/mgo/model/");
+		gen.addExcludedTable("db_version");
 		gen.generate();
 	}
 }
